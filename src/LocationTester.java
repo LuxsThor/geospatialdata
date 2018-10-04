82de9003e61389a816fe5626f170a20861c8d7a0
ruby -v <lucas.thorbecke@bluewin.ch>
2018-10-04 22:35:23 +0200

    some trials with classes

--- src/LocationTester.java
+++ src/LocationTester.java
@@ -1,13 +1,26 @@
 public class LocationTester {
     public double longitude;
     public double latitude;
+    private double secretmagic;
 
+    public LocationTester() {
+        this.longitude = 50.0;
+        this.latitude = 50.0;
+    }
+    // overloading the constructor
     public LocationTester(double lat, double lon) {
         this.latitude = lat;
         this.longitude = lon;
     }
 
-    public double distance(LocationTester other) {
-        return getDist(this.latitude, this.longitude, other.latitude, other.longitude);
+    public void setSecret(double secret){
+        this.secretmagic = secret;
     }
+
+    public double getSecret() {
+        return this.secretmagic;
+    }
+//    public double distance(LocationTester other) {
+//        return getDist(this.latitude, this.longitude, other.latitude, other.longitude);
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.sql.SQLOutput;
 
 public class Main {
 
     public static void main(String[] args) {
         LocationTester me = new LocationTester(33.4, 120.9);
+        LocationTester anotherme = new LocationTester();
         LocationTester goldtreasure = new LocationTester(80.5, 34.9);
 
-        System.out.println("Distance from me to treasure"+ me.distance(goldtreasure));
+        System.out.println("this is me " + me.latitude + " that is another me " + anotherme.latitude + " and that is the goldtreasure " + goldtreasure.latitude + " latitude");
+        me.latitude += 50.0;
+        System.out.println("this is the new me " + me.latitude);
+        me.setSecret(20.0);
+        System.out.println("my secret " + me.getSecret());
     }
 }
